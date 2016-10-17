e2f6af729b7ce16349679a69d43ed2a3eff2f817
Ricardo Job <sousajob@gmail.com>
2016-10-17 11:00:27 -0300

    instalando no ubuntu

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 sudo apt-get update
 sudo apt-get install apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+#open or created /etc/apt/sources.list.d/docker.list
+#add
 deb https://apt.dockerproject.org/repo ubuntu-xenial main
+
 sudo apt-get update
 sudo apt-get purge lxc-docker
 sudo apt-get update
 sudo apt-get install docker.io
-
-
